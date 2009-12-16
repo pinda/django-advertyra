c16746af1f8877bb5dc7969ffbf881da8f3cd563
Joeri Djojosoeparto <jsoejitno@gmail.com>
2009-12-16 16:34:42 +0100

    Removed artikelly

--- artikelly/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from artikelly.models import Article
-
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status')
-    list_filter = ('status', )
-
-admin.site.register(Article, ArticleAdmin)
