0e95d5a88faf2b82e182cdc535f52421001d3b43
Joeri Djojosoeparto <jsoejitno@gmail.com>
2009-12-24 15:55:46 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+ 
+setup(
+    name='advertyra',
+    description='Manage and create advertisements..',
+    author='Bread and Pepper',
+    author_email='joeri@breadandpepper.com',
+    url='http://github.com/pinda/django-advertyra',
+    packages=[
+        'advertyra',
+    ],
+)
