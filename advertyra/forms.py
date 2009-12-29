66c0ee417afb435488fe26639f5960d8a27dd752
Joeri Djojosoeparto <jsoejitno@gmail.com>
2009-12-29 16:37:55 +0100

    Enhancements for placeholder form

--- advertyra/forms.py
+++ advertyra/forms.py
@@ -2,45 +2,46 @@ import datetime
 from django import forms
 from django.utils.translation import ugettext as _
 
-from advertyra.models import Advertisement, Campaign
+from advertyra.models import Advertisement, Campaign, Placeholder
 
-def placeholder_taken(placeholder, ad_pk=None, cam_pk=None):
-    try:
-        campaign = Campaign.objects.exclude(pk=cam_pk).get(place=placeholder,
-                                        start__lte=datetime.datetime.now(),
-                                        end__gte=datetime.datetime.now())
-    except Campaign.DoesNotExist:
-        try:
-            ad = Advertisement.objects.exclude(pk=ad_pk).get(place=placeholder,
-                                                             visible=True)
-        except Advertisement.DoesNotExist:
-            return placeholder
-        else:
-            raise forms.ValidationError(_('Placeholder already taken by %s' % ad.title))
+def get_free_places():
+    """ Determine which placeholders are currently available """
+    ad_places = Advertisement.objects.filter(place__isnull=False).values("place__pk")
+    cam_places = Campaign.objects.filter(place__isnull=False).values("place__pk")
+
+    taken_places = []
+    for ad in ad_places:
+        taken_places.append(ad['place__pk'])
+    for ad in cam_places:
+        taken_places.append(ad['place__pk'])
+
+    places = Placeholder.objects.exclude(id__in=taken_places)
+
+    if places:
+        free_places = ((ad.pk, ad.title) for ad in Placeholder.objects.exclude(id__in=taken_places))
+        return free_places
     else:
-        raise forms.ValidationError(_('Placeholder already taken by %s' % campaign.title))
+        return (('0', _('No places available')),)
 
 class AdvertisementForm(forms.ModelForm):
+    place = forms.ChoiceField(choices=get_free_places())
+    
     model = Advertisement
 
     def clean_place(self):
-        try:
-            current_ad = Advertisement.objects.get(title__iexact=self.cleaned_data['title'])
-        except Advertisement.DoesNotExist:
-            return self.cleaned_data['place']
-        
-        if self.cleaned_data['place']:
-            return placeholder_taken(self.cleaned_data['place'], ad_pk=current_ad.pk)
+        if self.cleaned_data['place'] == "0":
+            raise forms.ValidationError(_('There are no free places, create one first'))
+        return Placeholder.objects.get(pk=self.cleaned_data['place'])
+
 
 class CampaignForm(forms.ModelForm):
+    place = forms.ChoiceField(choices=get_free_places())
+    
     model = Campaign
 
     def clean_place(self):
-        try:
-            current_cam = Campaign.objects.get(title__iexact=self.cleaned_data['title'])
-        except Campaign.DoesNotExist:
-            return self.cleaned_data['place']
-        
-        if self.cleaned_data['place']:
-            return placeholder_taken(self.cleaned_data['place'], current_cam.pk)
+        if self.cleaned_data['place'] == "0":
+            raise forms.ValidationError(_('There are no free places, create one first'))
+        return Placeholder.objects.get(pk=self.cleaned_data['place'])
+    
                                                
