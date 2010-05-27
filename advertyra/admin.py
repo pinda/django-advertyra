863b2082ef11085ea030729600ec20c696e4c5f0
Petar Radosevic <petar@wunki.org>
2010-05-27 11:25:05 +0200

    BUGFIX: Removed size from exclude

--- advertyra/admin.py
+++ advertyra/admin.py
@@ -9,14 +9,13 @@ from advertyra.utils import get_placeholders, clicks_for_ad
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ('title', 'place', 'visible' )
     list_filter = ('visible', )
-    exclude = ('size', )
     form = AdvertisementForm
 
     class Media:
         js = ('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js',
               'advertyra/js/jquery.flot.min.js',
               'advertyra/js/advertyra.plot.js')
-        
+
         css = {'all': ('advertyra/css/advertyra.css',) }
 
     def add_view(self, request, form_url='', extra_context=None):
@@ -26,7 +25,7 @@ class AdvertisementAdmin(admin.ModelAdmin):
 
     def change_view(self, request, object_id, extra_context=None):
         get_placeholders(request)
-        
+
         click_data = clicks_for_ad(object_id)
 
         month_list = Click.objects.filter(ad__pk=object_id).dates('datetime', 'month')
@@ -46,7 +45,7 @@ class CampaignAdmin(admin.ModelAdmin):
               'advertyra/js/advertyra.plot.js')
 
         css = {'all': ('advertyra/css/advertyra.css',) }
-    
+
     def add_view(self, request, form_url='', extra_context=None):
         get_placeholders(request)
 
@@ -59,17 +58,15 @@ class CampaignAdmin(admin.ModelAdmin):
         start_date = datetime.datetime.now().date().replace(day=1)
         end_date = start_date + datetime.timedelta(days=31)
         end_date.replace(day=1)
-        
+
         campaign = Campaign.objects.get(pk=object_id)
         month_list = Click.objects.filter(ad__pk__in=campaign.ad.all()).dates('datetime', 'month')
-        
+
         extra_context = {'month_list': month_list,
                          'start_date': start_date,
                          'end_date': end_date }
-        
+
         return super(CampaignAdmin, self).change_view(request, object_id, extra_context)
 
 admin.site.register(Advertisement, AdvertisementAdmin)
 admin.site.register(Campaign, CampaignAdmin)
-
-    
